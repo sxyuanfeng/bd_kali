df7e94e332c897e0e808b4684715e2e1f3157e59
sxyuanfengg <553426419@qq.com>
2020-03-15 17:54:13 +0800

    2020-03-15

--- channel/views.py
+++ channel/views.py
@@ -4,6 +4,7 @@ import os
 import json
 import pandas as pd
 import pymongo
+import jieba.analyse
 
 client = pymongo.MongoClient(host='localhost', port=27017)
 db = client.weibo
@@ -11,5 +12,16 @@ db = client.weibo
 def get_hottag(request):
     data = db.channel.find_one({"label": "热搜"}, {"list"})
     data = pd.DataFrame(data['list'])
-    print(data)
-    return HttpResponse('11')
+    tag_list = []
+    value = 51
+    for desc in data['desc']:
+        tag_line = jieba.analyse.extract_tags(desc, topK=2, allowPOS=('n'))
+        if len(tag_line) > 0:
+            for item in tag_line:
+                tag_list.append({'name': item, 'value': value})
+        else:
+            pass
+        value = value - 1
+    return HttpResponse(json.dumps(tag_list))
+
+
